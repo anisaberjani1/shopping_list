0e1af9d1b477c6da16a202a49babe753b3d6d6d2
Anisa Berjani <anisa.berjani@gmail.com>
2025-09-23 14:34:50 +0200

    Adding validation logic

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -15,8 +16,65 @@ class _NewItemState extends State<NewItem> {
     return Scaffold(
       appBar: AppBar(title: const Text('Add a new item')),
       body: Padding(
-          padding: const EdgeInsets.all(12),
-          child: Text('The form'),
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: InputDecoration(label: Text('Name')),
+                validator: (value) {
+                  if(value == null || value.isEmpty || int.tryParse(value) == null || int.tryParse(value)! <= 0){
+                    return 'Must be a valid, positive number.';
+                  }
+                  return null;
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(label: Text('Quantity')),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(width: 6),
+                                Text(category.value.title),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value){},
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 12,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(onPressed: (){}, child: const Text('Reset')),
+                  ElevatedButton(onPressed: (){}, child: const Text('Add item')),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
